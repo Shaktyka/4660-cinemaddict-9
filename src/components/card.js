ac1cd77a35ec959e16e05f71bc75156ccc8d6e4a
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-08-18 17:02:40 +0600

    Добавляет данные в карточку фильма

--- src/components/card.js
+++ src/components/card.js
@@ -14,9 +14,9 @@ export const makeCard = (dataObj) => {
       <p class="film-card__description">${cropText(dataObj.description.join(` `))}</p>
       <a class="film-card__comments">${dataObj.comments.length} comments</a>
       <form class="film-card__controls">
-        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
-        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
-        <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${dataObj.inWatchlist ? `film-card__controls-item--active` : ``}">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched ${dataObj.isWatched ? `film-card__controls-item--active` : ``}">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite ${dataObj.isFavorite ? `film-card__controls-item--active` : ``}">Mark as favorite</button>
       </form>
     </article>`.trim();
 };
